8dd03093a7909d7731fcd393d8cd9e8192bf2ab7
r.mustafin <r.mustafin@vk.team>
2022-10-20 12:46:01 +0300

    1381

--- 1381. Design a Stack With Increment Operation.go
+++ 1381. Design a Stack With Increment Operation.go
@@ -16,21 +16,42 @@
 package leetcode
 
 type CustomStack struct {
+	s       []int
+	maxSize int
 }
 
 func Constructor(maxSize int) CustomStack {
 
-	return CustomStack{}
+	return CustomStack{
+		s:       make([]int, 0, maxSize),
+		maxSize: maxSize,
+	}
 }
 
 func (cs *CustomStack) Push(x int) {
-
+	if len(cs.s) == cap(cs.s) {
+		return
+	}
+	cs.s = append(cs.s, x)
 }
 
 func (cs *CustomStack) Pop() int {
-	return 0
+	if len(cs.s) == 0 {
+		return -1
+	}
+
+	x := cs.s[len(cs.s)-1]
+
+	cs.s = cs.s[:len(cs.s)-1]
+
+	return x
 }
 
 func (cs *CustomStack) Increment(k int, val int) {
+	from := len(cs.s) - 1
+	to := from - k
 
+	for i := from; i >= to && i >= 0; i-- {
+		cs.s[i] += val
+	}
 }
